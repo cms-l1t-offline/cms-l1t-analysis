d1f9d17613d67348756b3ebec2f78a27f5098e00
Ben Krikler <b.krikler@cern.ch>
2017-06-05 15:48:37 +0200

    Fix histogram dimensions!

--- cmsl1t/hist/hist_collection.py
+++ cmsl1t/hist/hist_collection.py
@@ -42,8 +42,7 @@ class HistogramCollection(object):
      - needs to know how to create new histograms
     '''
 
-    def __init__(self, dimensions, histogram_factory, args_function = None,
-                 *vargs, **kwargs):
+    def __init__(self, dimensions, histogram_factory, *vargs, **kwargs):
         '''
             Should dimensions include or exclude histogram names?
         '''
@@ -52,58 +51,32 @@ class HistogramCollection(object):
         for dim in dimensions:
             if not isinstance(dim, BinningBase):
                 raise RuntimeError("non-Dimension object given to histogram")
-        self._dimensions = dimensions
+        self.__dimensions = dimensions
+        self.shape = tuple([ len(dim) for dim in dimensions ])
 
         if isinstance(histogram_factory, str):
             histogram_factory = HistFactory(histogram_factory, 
                                             *vargs, 
                                             **kwargs)
-        self._prepare_collection(histogram_factory, args_function)
+        self.values = self._prepare_collection(dimensions, histogram_factory)
 
-    def _prepare_collection(self, histogram_factory, args_function):
-        # Build the linked list of dimension bins objects
-        last_dim = None
-        for dimension in reversed(self._dimensions):
-            if not last_dim is None:
-                dimension.set_all_values(deepcopy(last_dim))
-            last_dim = dimension
-        #self._dimensions[0] = deepcopy(last_dim)
-	for i, dim in enumerate(self._dimensions):
-		print("BEK dimensions:",i, dim)
-
-        # Now recurse over all dimensions and create the actual histograms
-        self._recurse_build_hists( self._dimensions,
-                histogram_factory, args_function=args_function)
-
-    def _recurse_build_hists(self, 
-                             dimensions,
-                             histogram_factory,
-                             args_function=None,
-                             bin_labels=[],
-                             depth=0
-                             ):
-        print("BEK: depth", depth)
-        print("BEK: dimensions", dimensions)
-        this_dim = dimensions[0]
-        if len(dimensions) == 1:
-            for bin in this_dim.iter_all():
-                vargs = bin_labels + [bin]
-                print("BEK: _recurse_build_hists bin", bin,this_dim.values[bin])
-                print("BEK: _recurse_build_hists vargs", vargs)
-                kwargs = {}
-                if args_function:
-                    vargs, kwargs = args_function(bin_labels)
-                hist = histogram_factory(*vargs, **kwargs)
-                this_dim.set_value(bin, hist)
-        else:
+    def _prepare_collection(self, dimensions, histogram_factory,  bin_labels=[], depth=0):
+            this_dim = deepcopy(dimensions[0])
             remaining_dims = dimensions[1:]
             for bin in this_dim.iter_all():
-                print("BEK: _recurse_build_hists recursing: bin", bin,this_dim.values[bin])
-                passed_bin_labels =  bin_labels + [bin]
-                self._recurse_build_hists(
-                        remaining_dims, histogram_factory,
-                        args_function, passed_bin_labels,depth+1)
-
+                if remaining_dims:
+                    value = self._prepare_collection(
+                            remaining_dims,
+                            histogram_factory,
+                            bin_labels + [bin] ,
+                            depth + 1)
+                    this_dim.set_value(bin, value)
+                else:
+                    vargs = bin_labels + [bin]
+                    kwargs = {}
+                    hist = histogram_factory(*vargs, **kwargs)
+                    this_dim.set_value(bin, hist)
+            return this_dim
 
     @classmethod
     def _flatten_bins(self, bins):
@@ -131,7 +104,7 @@ class HistogramCollection(object):
 
         # Check every dimension if it contains these values
         bins = []
-        for key, dimension in zip(keys, self._dimensions[:n_keys]):
+        for key, dimension in zip(keys, self.__dimensions[:n_keys]):
             bins.append(dimension.find_bins(key))
 
         # Some dimensions might return multiple values, flatten returned arrays
@@ -140,12 +113,11 @@ class HistogramCollection(object):
         return bins
 
     def get_bin_contents(self, bin_list):
-        if isinstance(bin_list, int):
+        if isinstance(bin_list, (str, int)):
             bin_list = [bin_list]
-        value = self._dimensions[0]
-	print("BEK len(bin_list)",len(bin_list), bin_list)
+        #value = self.__dimensions[0]
+        value = self.values
         for index in bin_list:
-            print("BEK value",value)
             value = value.get_bin_contents(index)
         return value
 
@@ -157,25 +129,23 @@ class HistogramCollection(object):
                 coll[x, y, z]
         '''
         bin_indices = self._find_bins(keys)
-        print("BEK bin_indices:",bin_indices)
         objects = [self.get_bin_contents(bins) for bins in bin_indices]
         return HistCollectionView(objects)
 
     def shape(self):
-        _shape = [len(dim) for dim in self._dimensions]
-        return tuple(_shape)
+        return self.shape
 
     def __len__(self):
-        return len(self._dimensions[0])
+        return len(self.__dimensions[0])
 
     def __iter__(self):
         # # In python >3.3 we should do
-        # yield from self._dimensions[0]
-        for bin in self._dimensions[0]:
+        # yield from self.__dimensions[0]
+        for bin in self.__dimensions[0]:
             yield bin
 
     def iter_all(self):
         # # In python >3.3 we should do
-        # yield from self._dimensions[0]
-        for bin in self._dimensions[0].iter_all():
+        # yield from self.__dimensions[0]
+        for bin in self.__dimensions[0].iter_all():
             yield bin
