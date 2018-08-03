414e93f6f8cb0dbe562b50abc6894849c6635fba
kreczko <lkreczko@googlemail.com>
2018-08-03 19:01:35 +0100

    pruned conda package list

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,4 +1,4 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 # In Python 3.4, we test the min version of NumPy and SciPy. In Python 2.7, we test more recent version.
 if test -e ${CMSL1T_CONDA_PATH}/envs/cms; then
     echo "cms env already exists."
@@ -8,19 +8,15 @@ else
 fi
 
 source activate cms
-conda update conda -yq
-conda update pip -yq
-conda install psutil -yq
 conda config --add channels http://conda.anaconda.org/NLeSC
 conda config --set show_channel_urls yes
 
 conda install -y -q \
   matplotlib \
   numpy \
-  pandas==0.23 \
   root>=6.04 \
-  root-numpy \
   rootpy
 
+pip install -U pip
 pip install -r requirements.txt
 source deactivate
