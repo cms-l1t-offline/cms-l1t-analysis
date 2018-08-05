3e46f027faa24be61439b768c0271519efb75398
Lukasz Kreczko <lkreczko@googlemail.com>
2018-08-05 13:40:11 +0100

    disallowing errors in CI install

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-# In Python 3.4, we test the min version of NumPy and SciPy. In Python 2.7, we test more recent version.
+set -e
 if test -e ${CMSL1T_CONDA_PATH}/envs/cms; then
     echo "cms env already exists."
 else
     echo "Creating cms env."
-    conda install -y -q psutil
     conda create --yes -q -n cms python=${TRAVIS_PYTHON_VERSION}
 fi
 
 source activate cms
+conda install -y -q psutil
+
 conda config --add channels http://conda.anaconda.org/NLeSC
 conda config --set show_channel_urls yes
 
@@ -21,3 +22,4 @@ conda install -y -q \
 pip install -U pip
 pip install -r requirements.txt
 source deactivate
+set +e
