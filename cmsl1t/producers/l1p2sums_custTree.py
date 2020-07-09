c1345c6a416bb1bf9b04a2ac5c231c55a7a9d059
Aaron Bundock <aaron.bundock@cern.ch>
2020-07-09 19:26:22 +0200

    Adding updates for using custom pf jets & sums L1Ntuple branch

---
+++ cmsl1t/producers/l1p2sums_custTree.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+from cmsl1t.energySums import EnergySum, Met
+from .base import BaseProducer
+
+
+class Producer(BaseProducer):
+
+    def __init__(self, inputs, outputs, **kwargs):
+        self._expected_input_order = ['phaseIPFJetHt', 'pfMet']
+        super(Producer, self).__init__(inputs, outputs, **kwargs)
+
+    def produce(self, event):
+        setattr(event, self._outputs[0] + '_Met', Met(event['L1PhaseIPFJet_pfMet'], 0.))
+        setattr(event, self._outputs[0] + '_MetHF', Met(event['L1PhaseIPFJet_pfMet'], 0.))
+        setattr(event, self._outputs[0] + '_Htt', EnergySum(event['L1PhaseIPFJet_phaseIPFJetHt']))
+
+        return True
