488a32d20407eb095491edbe9f44181377c18a3e
Luke Kreczko <lkreczko@googlemail.com>
2020-11-09 21:41:31 +0000

    make remove_from_path Python 3 compatible

--- bin/remove_from_path.py
+++ bin/remove_from_path.py
@@ -3,6 +3,7 @@
         python remove_from_env.py $PATH $TO_BE_REMOVED
     returns $PATH without paths starting with $TO_BE_REMOVED
 '''
+from __future__ import print_function
 import sys
 
 ENV = sys.argv[1]
@@ -13,4 +14,4 @@ for path in ENV.split(':'):
     if path.startswith(REMOVE):
         continue
     new_path.append(path)
-print ':'.join(new_path)
+print(':'.join(new_path))
